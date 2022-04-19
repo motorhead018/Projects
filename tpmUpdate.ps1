4285186e84f18e1acbca7dd5afb19607fda2f5e9
motorhead018 <38567415+motorhead018@users.noreply.github.com>
2022-04-19 11:29:27 -0500

    commit to main

--- tpmUpdate.ps1
+++ tpmUpdate.ps1
@@ -1,3 +1,6 @@
+#This script can be used to update the TPM firmware on HP devices
+#Used in conjunction with the PDF "A User Guide for TPM Config and HpqPswd.pdf"
+
 #Command line syntax: TpmConfig -s -aTPMSpecVersion -pBiosPasswordFile
 
 tpmconfig64.exe -s -a2.0 -p
