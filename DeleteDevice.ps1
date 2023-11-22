6f8603171d7c95cf0db7975886979f347a765be7
motorhead018 <38567415+motorhead018@users.noreply.github.com>
2023-11-22 17:16:53 -0600

    save progress

--- DeleteDevice.ps1
+++ DeleteDevice.ps1
@@ -2,6 +2,7 @@
 # This script connects to the Configuration Manager AdminService and deletes the device with the ResourceID "ResourceID"
 
 $AdminServiceUrl = "https://SMSProviderFQDN/AdminService"
+$SerialNumber = Get-WmiObject win32_bios | Select-Object -ExpandProperty SerialNumber
 $ResourceID = "ResourceID"
 
 $DeleteDeviceRequest = New-Object System.Net.Http.HttpRequestMessage(HttpMethod.Delete, "$AdminServiceUrl/wmi/SMS_Device/$ResourceID")
