0139ece3ede6a4c135c63c18a101d84e680ec939
motorhead018 <38567415+motorhead018@users.noreply.github.com>
2023-11-22 17:16:34 -0600

    First time upload

---
+++ ConfigMgrWebServiceMethods.ps1
@@ -0,0 +1,7 @@
+﻿# Connect to web service
+$URI = "http://localhost/ConfigMgrWebService/ConfigMgr.asmx"
+$Secret = "f1fb920f-c710-42ac-80ad-bf505512b66f"
+$Web = New-WebServiceProxy -Uri $URI
+
+# Invoke method
+#$Web.RemoveCMDeviceByName("yoursecretkey", "username")#Save methods to variable$WebService = $Web
