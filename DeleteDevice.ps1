a462eeb97ded29a7644f2dde36f9a50ce7f047f7
motorhead018 <38567415+motorhead018@users.noreply.github.com>
2023-11-22 13:17:50 -0600

    Create DeleteDevice.ps1
    Script created with Google Bard. The script will delete a device(s) in ConfigMgr via the AdminService.

---
+++ DeleteDevice.ps1
@@ -0,0 +1,15 @@
+# Delete a device from Configuration Manager
+# This script connects to the Configuration Manager AdminService and deletes the device with the ResourceID "ResourceID"
+
+$AdminServiceUrl = "https://SMSProviderFQDN/AdminService"
+$ResourceID = "ResourceID"
+
+$DeleteDeviceRequest = New-Object System.Net.Http.HttpRequestMessage(HttpMethod.Delete, "$AdminServiceUrl/wmi/SMS_Device/$ResourceID")
+$DeleteDeviceRequest.Headers.Authorization = New-Object System.Net.Http.Headers.AuthenticationHeaderValue("Basic", [System.Text.Encoding]::ASCII.GetBytes("username:password") | ConvertTo-Base64String)
+
+$DeleteDeviceResponse = Invoke-RestMethod -Method Delete -Uri $DeleteDeviceRequest.RequestUri
+if ($DeleteDeviceResponse.StatusCode -eq 204) {
+    Write-Output "Device deleted successfully."
+} else {
+    Write-Output "Error deleting device. Status code: $DeleteDeviceResponse.StatusCode"
+}
